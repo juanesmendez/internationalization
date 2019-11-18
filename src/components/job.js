82c23734655405ebeb8c64bd9ecb2266630652a1
Juan Esteban Méndez <je.mendez@uniandes.edu.co>
2019-11-18 13:56:07 -0500

    Se adelanto en internacionalizacion.

--- src/components/job.js
+++ src/components/job.js
@@ -1,17 +1,39 @@
 import React from 'react';
+import { FormattedDate, FormattedNumber, FormattedPlural } from 'react-intl';
 
 export default class Job extends React.Component {
 
-  	render() {
-  		return (
-  			<tr>
-  				<th scope="row">{this.props.offer.id}</th>
-      			<td>{this.props.offer.name}</td>
-  				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
-      			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
-  			</tr>
-  		);
+	render() {
+		return (
+			<tr>
+				<th scope="row">{this.props.offer.id}</th>
+				<td>{this.props.offer.name}</td>
+				<td>{this.props.offer.company}</td>
+				<td>
+					{this.props.offer.salary} &nbsp;
+					<FormattedPlural
+						value={this.props.offer.salary}
+						one="Million"
+						other="Millones" />
+
+				</td>
+				<td>{this.props.offer.city}</td>
+				<td>
+					<FormattedDate
+						value={new Date(this.props.offer.date)}
+						year='numeric'
+						month='long'
+						day='numeric'
+						weekday='long'
+					/>
+				</td>
+				<td>
+					<FormattedNumber
+						value={this.props.offer.numvisits}
+
+					/>
+				</td>
+			</tr>
+		);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { IntlProvider } from 'react-intl';
 
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+
+function language() {
+    if (navigator.language.includes("es")) {
+        return localeEsMessages;
+    } else if (navigator.language.includes("en")) {
+        return localeEnMessages;
+    }
+
+}
+
+ReactDOM.render(
+    <IntlProvider locale={navigator.language} messages={language()}>
+        <JobsList />
+    </IntlProvider>, document.getElementById("root")
+);
+
